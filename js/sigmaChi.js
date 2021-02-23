5748355fe77a664492f76e1e1801f69b31b758b6
Connor St. Louis <15cmcs@queensu.ca>
2021-02-23 18:47:17 -0500

    Updated Functions in .js

--- js/sigmaChi.js
+++ js/sigmaChi.js
@@ -25,12 +25,100 @@ function Data_Points_With_Uncertainty(data, uncertainties){
   return dataWithUncertanties;
 }
 
-function weighted_Mean(){
-  alert("Hello World");
+//function returns the weighted mean based on data points with uncertainty being used as the weight
+//the argument allData should be an array returned directly from Data_Points_With_Uncertainty
+//documentation on function implemented can be found here: http://seismo.berkeley.edu/~kirchner/Toolkits/Toolkit_12.pdf
+//THIS FUNCTION IS CURRENTLY UNTESTED
+//THIS FUNCTION IS CURRENTLY UNTESTED
+//THIS FUNCTION IS CURRENTLY UNTESTED
+function weighted_Mean(allData){
+  var weightedMean = 0.0;
+  var sumOfWeights = 0.0;
+  //for each data point
+  for (i = 0; i < data.length; i++){
+    //sum of all data points * their respective weights
+    weightedMean += (allData[i][0] * (allData[i][2] - allData[i][1]));
+    sumOfWeights += (allData[i][2] - allData[i][1]);
+  }
+  weightedMean = weightedMean / sumOfWeights;
+  return weightedMean;
 }
 
-function reduced_Chai_Squared(){
-  alert("Hello World");
+//function returns the weighted mean variance
+//the argument allData should be an array returned directly from Data_Points_With_Uncertainty
+//documentation on function implemented can be found here: http://seismo.berkeley.edu/~kirchner/Toolkits/Toolkit_12.pdf
+//THIS FUNCTION IS CURRENTLY UNTESTED
+//THIS FUNCTION IS CURRENTLY UNTESTED
+//THIS FUNCTION IS CURRENTLY UNTESTED
+function weighted_Mean_Variance(allData){
+  var weightedMeanVariance = 0.0;
+  var weight = 0.0
+  var sumOfWeights = 0.0;
+  //for each data point
+  for (i = 0; i < data.length; i++){
+    //w(x-weightedMean)^2
+    weight = (allData[i][2] - allData[i][1]);
+    weightedMeanVariance += (weight * ((allData[i][0] - weightedMean(allData)) * (allData[i][0] - weightedMean(allData))));
+    sumOfWeights += weight;
+  }
+  weightedMeanVariance = weightedMeanVariance / sumOfWeights;
+  weightedMeanVariance = weightedMeanVariance * (data.length / (data.length - 1))
+  return weightedMeanVariance;
+}
+
+//function returns the weighted mean uncertainty
+//the argument allData should be an array returned directly from Data_Points_With_Uncertainty
+//documentation on function implemented can be found here: http://seismo.berkeley.edu/~kirchner/Toolkits/Toolkit_12.pdf
+//THIS FUNCTION IS CURRENTLY UNTESTED
+//THIS FUNCTION IS CURRENTLY UNTESTED
+//THIS FUNCTION IS CURRENTLY UNTESTED
+function weighted_Mean_Uncertainty(allData){
+  var weightedMeanUncertanty = 0.0;
+  Math.sqrt(weighted_Mean_Variance(allData) / allData.length);
+  return;
+}
+
+//function returns the expected value (average)
+//FUNCTION IS CURRENTLY UNTESTED
+function Expected_Value(data){
+  var sum = 0;
+  var avg = 0;
+  var len = data.length;
+  for (i=0; i < len; i++){
+    sum += data[i][0];
+  }
+  avg = sum/len;
+  return avg;
+}
+//function reutrns the chi_squared of a dataset
+//function uses Expected_Value(data)
+//FUNCTION IS CURRENTLY UNTESTED
+function chi_squared (data){
+  var chi = 0; //place holder for the chi-square value
+  chi-sqr = new Array();
+  var numerator = 0;
+  var ev = Expected_Value(data); //expected value for data
+
+  for (i = 0; i < data.length; i++) {
+    numerator = Math.pow(data[i][0],ev);
+    chi-sqr[i] = numerator/ev;
+  }
+  return chi-sqr;
+}
+
+//function will calculate the reduced chi squared of a dataset
+//rejected = number of rejected datapoints
+//fucntion uses Expected_Value(data)
+//function uses chi_squared(data)
+//FUNCTION IS CURRENTLY UNTESTED
+function reduced_Chai_Squared(data, rejected){
+  var accepted = data.length - rejected;  //accepted = degrees of freedom
+  var reduced-chi-sqr = new Array();
+  var chi = chi_squared(data);
+  for (i = 0; i < data.length; i++) {
+    reduced-chi-sqr[i] = chi[i] / accepted;
+  }
+  return reduced-chi-sqr;
 }
 
 function univariate_Kernel_Density(){
