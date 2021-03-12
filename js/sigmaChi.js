4aa00933939d325eb11852f26b4ba7b690c31de5
Tyler Beckstead <becker1998.tb@gmail.com>
2021-03-12 11:03:47 -0500

    can detect if checkbox is checked

--- js/sigmaChi.js
+++ js/sigmaChi.js
@@ -373,7 +373,7 @@ function addNewData (){
   var newCheckBox = document.createElement("input");
   newCheckBox.className = "text-center";
   newCheckBox.setAttribute("type", "checkbox");
-  newCheckBox.setAttribute("onchange", "graph()");
+  newCheckBox.setAttribute("onchange", "graph(this)");
   var checkboxID = "checkdata" + numLabels;
   newCheckBox.setAttribute("id", checkboxID);
 
@@ -667,7 +667,12 @@ function getLabels (data){
   return labels;
 }
 
-function graph (){
+function graph (input){
+  var iD = input.id;
+  var check = document.getElementById(iD);
+  if (check.checked == true){
+    console.log("Checkbox is Checked!!");
+  }
   graphKernelDensity();
   grapghReducedChiSquared();
 }
@@ -717,43 +722,3 @@ function graphKernelDensity(){
   }
 });
 }
-function csvTest() {
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }]
-          }
-      }
-  });
-}
-//csvTest();
