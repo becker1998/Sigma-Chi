01b587499015adb8ee3b33b5c4e4340a8a8a1b6d
Tyler Beckstead <becker1998.tb@gmail.com>
2021-03-13 21:27:59 -0500

    Update sigmaChi.js

--- js/sigmaChi.js
+++ js/sigmaChi.js
@@ -23,6 +23,18 @@ var eWtdAvg = 0;
 var eFunction = 0;
 var eBandwidth = 0;
 
+//Global array for possible colours to use for grahping lines
+var colours = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
 function updateEvaluationSettingsUncertainty(){
   var selectBox = document.getElementById("uncertaintySelection");
   var selectedValue = selectBox.options[selectBox.selectedIndex].value;
@@ -648,6 +660,7 @@ function graph (input){
     else{
       //alert("Please only select on dataset to graph");
       graphMultipleReducedChiSquared();
+      graphMultipleKernelDensity();
       //check.checked = false;
     }
   }
@@ -694,21 +707,15 @@ function graphMultipleReducedChiSquared(){
   var graphData = new Array();
   for (var i = 0; i < getChecked.length; i++) {
     var tempdata = Data_Points_With_Uncertainty(datasets[getChecked[i]],datasetsUncer[getChecked[i]],false);
-    console.log("tempData: ");
-    console.log(tempdata);
     var tempChi = reduced_Chai_Squared(tempdata,0);
-    console.log("tempChi: " + tempChi);
     var tempX = {
       data: tempChi,
       label: datasetsName[i],
+      borderColor : colours[i],
       fill: false
     };
     graphData.push(tempX);
   }
-  console.log("labels: ");
-  console.log(dataLabels);
-  console.log("Datasets: ");
-  console.log(graphData);
   var multiSqrContext = document.getElementById('rcSqr').getContext('2d');
   var squareChart = new Chart(multiSqrContext,{
   type: 'line',
@@ -764,4 +771,31 @@ function graphKernelDensity(){
     ]
   }
 });
+}
+function graphMultipleKernelDensity(){
+  var getChecked = getCheckedDatasets();
+  var maxDataset = getMaxDatasetLength(getChecked);
+  var dataLabels = getLabels(datasets[maxDataset]);
+  var datasetsName = getCheckedDatasetsName();
+  var bandwidth = 0.8333333333333334;
+  var graphData = new Array();
+  for (var i = 0; i < getChecked.length; i++) {
+    var tempdata = Data_Points_With_Uncertainty(datasets[getChecked[i]],datasetsUncer[getChecked[i]],false);
+    var tempKer = univariate_Kernel_Density(bandwidth,tempdata,true);
+    var tempX = {
+      data: tempKer,
+      label: datasetsName[i],
+      borderColor : colours[i],
+      fill: false
+    };
+    graphData.push(tempX);
+  }
+  var multiKerContext = document.getElementById('kerDest').getContext('2d');
+  var squareChart = new Chart(multiKerContext,{
+  type: 'line',
+  data: {
+    labels: dataLabels,
+    datasets: graphData
+    }
+  });
 }
