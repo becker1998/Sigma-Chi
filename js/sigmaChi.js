cdc9adee84f2cf100b455762e5c265888915b202
Connor St. Louis <15cmcs@queensu.ca>
2021-03-31 14:23:58 -0400

    not needed

--- js/sigmaChi.js
+++ js/sigmaChi.js
@@ -154,8 +154,8 @@ function updateEvaluationSettings(firstGo) {
 }
 
 function forceUpdate(id) {
+  console.log("forceUpdate");
   var dataID = "checkdata" + tracker;
-  dynamicGraph(dataID);
   updateEvaluationSettings(false);
 }
 
